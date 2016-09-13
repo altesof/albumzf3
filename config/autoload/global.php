3f99ee8bb136b6bec7c4cac807340349c7137e07
miro <miro@sam.local>
2016-09-13 10:23:09 +0200

    Album cambio de base de datos de sqlite a mysql

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,8 +1,25 @@
 <?php
 
 return [
+    'db' => [
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=db_albumzf2;host=localhost',
+        'driver_options' => [
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+        ],
+    ],
+    'service_manager' => [
+        'factories' => [
+            'Zend\Db\Adapter\Adapter'
+                    => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ],
+    ],
+];
+/*
+return [
   'db' => [
       'driver' => 'Pdo',
       'dsn'    => sprintf('sqlite:%s/data/zftutorial.db', realpath(getcwd())),
   ],
 ];
+*/
